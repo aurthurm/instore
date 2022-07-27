18dad9471174a230f9bab7a4e1a67fc7996d5d0b
Aurthur Musendame <aurthurmusendame@gmail.com>
2022-07-27 18:45:05 +0200

    removed helmet

--- src/main.ts
+++ src/main.ts
@@ -21,21 +21,21 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  app.use(helmet({
-    crossOriginResourcePolicy: true,
-    crossOriginEmbedderPolicy: true,
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        mediaSrc: ["'self'", "'unsafe-inline'"],
-        scriptSrcElem: ["'self'", "'unsafe-inline'"],
-        scriptSrcAttr: ["'self'", "'unsafe-inline'"],
-        connectSrc: ["'self'", "'unsafe-inline'"],
-      }
-    }
-  }));
+  // app.use(helmet({
+  //   crossOriginResourcePolicy: true,
+  //   crossOriginEmbedderPolicy: true,
+  //   contentSecurityPolicy: {
+  //     useDefaults: true,
+  //     directives: {
+  //       scriptSrc: ["'self'", "'unsafe-inline'"],
+  //       styleSrc: ["'self'", "'unsafe-inline'"],
+  //       mediaSrc: ["'self'", "'unsafe-inline'"],
+  //       scriptSrcElem: ["'self'", "'unsafe-inline'"],
+  //       scriptSrcAttr: ["'self'", "'unsafe-inline'"],
+  //       connectSrc: ["'self'", "'unsafe-inline'"],
+  //     }
+  //   }
+  // }));
 
   await app.listen(
     process.env.BACKEND_SERVER_PORT || 5000,
