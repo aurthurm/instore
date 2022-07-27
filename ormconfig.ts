6af084ed4539185ac24d6061bdb920d16f9dd0f6
Aurthur Musendame <aurthurmusendame@gmail.com>
2022-07-27 14:48:30 +0200

    added postgres db name to env

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,11 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 
 const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: `${process.env.POSTGRES_DATABASE_NAME}`,
+  host: `${process.env.POSTGRES_DATABASE_HOST}`,
   port: +process.env.POSTGRES_DATABASE_PORT,
   username: `${process.env.POSTGRES_DATABASE_USER}`,
   password: `${process.env.POSTGRES_DATABASE_PASSWORD}`,
-  database: `${process.env.POSTGRES_DATABASE_DATABASE}`,
+  database: `${process.env.POSTGRES_DATABASE_NAME}`,
   entities: [__dirname + '../**/*.entity{.ts,.js}'],
   synchronize: true, // Must be false in production mode
 };
